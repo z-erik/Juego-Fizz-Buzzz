237d7785faadf93a7f83b96583f718a6eb03acc3
z-erik <erikp2913@gmail.com>
2025-11-19 11:10:45 -0600

    feat: logica terminada

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 let Fiz=0;
 
 let Buzz=0;
@@ -6,10 +7,23 @@ let FizzBuzz=0;
 
 for(i=1;i<=1000;i++){
 
-    
+    if(i%3==0 && i%5==0){
+    console.log("FizzBuzz");
+    FizzBuzz++;
+    }
+    else if(i%3==0){
+       console.log("Fizz"); 
+       Fiz++;
+    }
+    else if(i%5==0){
+       console.log("Buzz"); 
+       Buzz++;
+    }else{
         console.log(i);
-    
+    }
 
 
 
 }
+
+console.log("|Total FizzBuzz "+FizzBuzz+ "| Total Fizz "+Fiz+ " |Total Buzz "+Buzz)
